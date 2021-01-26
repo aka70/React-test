8ed23ad7411ffabc309f34b687558a39b88e6657
satoshi <akatsukisatoshi@gmail.com>
2021-01-26 22:30:12 +0900

    index.tsをindex.tsxに

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React, { StrictMode } from 'react';
+import ReactDOM from 'react-dom';
+import App from '/App';
+
+import 'semantic-ui-css/semantic.min.css';
+
+ReactDOM.render(
+    <StrictMode>
+        <App />
+    </StrictMode>,
+    document.getElementById('root'),
+);
