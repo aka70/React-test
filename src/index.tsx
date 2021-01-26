c282c33031c1468f47e100bdea1d99a6cf6046f7
satoshi <akatsukisatoshi@gmail.com>
2021-01-26 23:57:04 +0900

    ページ完成

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
-import React, { StrictMode } from 'react';
-import ReactDOM from 'react-dom';
-import App from '/App';
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-import 'semantic-ui-css/semantic.min.css';
+import "semantic-ui-css/semantic.min.css";
 
 ReactDOM.render(
-    <StrictMode>
-        <App />
-    </StrictMode>,
-    document.getElementById('root'),
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById("root")
 );
